81d95b4cb6db198beb948e91ad823abf98f71033
Nowa <135564143+Nowa771@users.noreply.github.com>
2024-06-13 18:59:32 +1000

    Added assets and created water room
    Created water room and made it so it adds water to inventory when placed.

--- Assets/Scripts/World scripts/Room.cs
+++ Assets/Scripts/World scripts/Room.cs
@@ -1,36 +1,26 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Room : MonoBehaviour
 {
     public float interval = 5f;
-    public int foodAmount = 1; 
-    public int waterAmount = 1; 
+    public int foodAmount = 1;
+    public int waterAmount = 1;
     public int materialAmount = 1;
 
-    private bool isAddingResources = false;
-
-    private void OnTriggerEnter(Collider other)
+    private void Start()
     {
-        if (other.CompareTag("Player"))
-        {
-            if (!isAddingResources)
-            {
-                StartCoroutine(AddResourcesOverTime());
-            }
-        }
+        StartCoroutine(AddResourcesOverTime());
     }
 
     private IEnumerator AddResourcesOverTime()
     {
-        isAddingResources = true;
         while (true)
         {
             yield return new WaitForSeconds(interval);
-            Inventory.Instance.AddFood(foodAmount);
-            Inventory.Instance.AddWater(waterAmount);
-            Inventory.Instance.AddMaterials(materialAmount); 
+            Inventory.Instance.AddFood(foodAmount); // food amount
+            Inventory.Instance.AddWater(waterAmount); // water amount
+            Inventory.Instance.AddMaterials(materialAmount); // material amount
 
             Debug.Log("Resources added to inventory.");
         }
